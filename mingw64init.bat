98aa71fee26d232dd4ba0008b92c0917dbb77934
ywsa-dev <ywsadev@gmail.com>
2026-07-19 20:13:35 +0900

    압축파일 삭제 기능 추가

--- mingw64init.bat
+++ mingw64init.bat
@@ -1,3 +1,4 @@
 @echo off
 curl -L -o mingw.zip https://github.com/brechtsanders/winlibs_mingw/releases/download/14.2.0posix-18.1.8-12.0.0-msvcrt-r1/winlibs-x86_64-posix-seh-gcc-14.2.0-mingw-w64msvcrt-12.0.0-r1.zip
 tar -xf mingw.zip
+del mingw.zip
